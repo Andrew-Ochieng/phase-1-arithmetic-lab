211164e12282727be05911981dafaa9a8e7dde8a
AndrewMartial <andrewomosh44@gmail.com>
2022-08-16 15:42:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+//multiply
+const num1 = 31;
+const num2 = 2;
+
+let multiply = parseInt(num1 * num2)
+multiply;
+
+//random
+let random = Math.floor(Math.random() * 10 + 1)
+
+//mod
+const num3 = 19
+const num4 = 5
+
+let mod = num3 % num4
+
+// max
+let max = Math.max(2, 6, 20, 15, 17, 4)
+
+
+
+
